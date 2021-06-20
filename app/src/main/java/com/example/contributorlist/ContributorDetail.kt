47cfb1ec7acdd01a477026473f613accada9a8d9
tadotcom <peertopeer1988@gmail.com>
2021-06-21 01:30:56 +0900

    リストの詳細を表示するフラグメントファイルの追加、フラグメントの表示

--- app/src/main/java/com/example/contributorlist/ContributorDetail.kt
+++ app/src/main/java/com/example/contributorlist/ContributorDetail.kt
@@ -8,5 +8,11 @@ class ContributorDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contributor_detail)
+
+        //フラグメントの表示
+        val contributorListFragment = ContributorListFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.contributor_detail_fragment, contributorListFragment)
+        fragmentTransaction.commit()
     }
 }
