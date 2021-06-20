4678ccc2fd7f15c3b94c897fe563cf9414da50e6
tadotcom <peertopeer1988@gmail.com>
2021-06-20 20:57:06 +0900

    非同期処理のためAsyncTaskを実装

--- app/src/main/java/com/example/contributorlist/Asynchronous.kt
+++ app/src/main/java/com/example/contributorlist/Asynchronous.kt
@@ -1,4 +1,11 @@
 package com.example.contributorlist
 
-class Asynchronous {
+import android.os.AsyncTask
+
+class Asynchronous : AsyncTask<Int?, Int?, Int>() {
+
+    //非同期処理を実行
+    override fun doInBackground(vararg p0: Int?): Int {
+        return 0
+    }
 }
