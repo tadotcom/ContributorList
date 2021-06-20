75c9cc867a5f68e69985f90e7681a200b3f6e226
tadotcom <peertopeer1988@gmail.com>
2021-06-21 01:05:53 +0900

    jsonを読みこみ、リストに表示する

--- app/src/main/java/com/example/contributorlist/Asynchronous.kt
+++ app/src/main/java/com/example/contributorlist/Asynchronous.kt
@@ -1,12 +1,14 @@
 package com.example.contributorlist
 
 import android.os.AsyncTask
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class Asynchronous : AsyncTask<Int?, Int?, Int>() {
 
     //非同期処理を実行
     override fun doInBackground(vararg p0: Int?): Int {
-
 
         return 0
     }
--- app/src/main/java/com/example/contributorlist/ContributorListFragment.kt
+++ app/src/main/java/com/example/contributorlist/ContributorListFragment.kt
@@ -1,14 +1,20 @@
 package com.example.contributorlist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ContributorListFragment : Fragment() {
+    var array = arrayOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,16 +27,24 @@ class ContributorListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 配列の生成
-        val array = arrayOf("リスト１", "リスト２", "リスト３", "リスト４", "リスト５")
+        // jsonファイルの読み込み
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("contributors.json")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        val str: String = bufferedReader.readText()
+        val jObject = JSONObject(str)
+        val jArray = jObject.getJSONArray("contributors")
 
-        // xmlにて実装したListViewの取得
-        val listView = view.findViewById<ListView>(R.id.list)
+        for (i in 1 until jArray.length()) {
+            val obj = jArray.getJSONObject(i)
+            val title = obj.getString("login")
+            val url = obj.getString("url")
 
-        // ArrayAdapterの生成
-        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, array)
+            array += title
 
-        // ListViewに、生成したAdapterを設定
-        listView.adapter = adapter
+            val listView = view.findViewById<ListView>(R.id.list)
+            val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, array)
+            listView.adapter = adapter
+        }
    }
 }
