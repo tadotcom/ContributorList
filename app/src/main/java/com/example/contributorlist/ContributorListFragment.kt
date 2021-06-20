5a139e94da1b08a90283a3833529f1188b057485
tadotcom <peertopeer1988@gmail.com>
2021-06-20 18:28:09 +0900

    ContributorListFragmentにRecycleViewを表示

--- app/src/main/java/com/example/contributorlist/ContributorListFragment.kt
+++ app/src/main/java/com/example/contributorlist/ContributorListFragment.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ContributorListFragment : Fragment() {
+    //lateinit val recyclerView :RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.list_fragment, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
     }
 }
