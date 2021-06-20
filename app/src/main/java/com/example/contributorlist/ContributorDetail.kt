19b3df7679b945610f897b1403cbf6d693e72b76
tadotcom <peertopeer1988@gmail.com>
2021-06-21 01:23:38 +0900

    リストの詳細を表示するcontributor_detailを追加

--- app/src/main/java/com/example/contributorlist/ContributorDetail.kt
+++ app/src/main/java/com/example/contributorlist/ContributorDetail.kt
@@ -1,6 +1,12 @@
 package com.example.contributorlist
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ContributorDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contributor_detail)
+    }
 }
