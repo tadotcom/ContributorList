d736627a2222d7be0d59b840eb02b6eda5ed9b16
tadotcom <peertopeer1988@gmail.com>
2021-06-20 17:54:15 +0900

    RecycleViewのアダプターの振る舞いを追加

--- app/src/main/java/com/example/contributorlist/CustomAdapter.kt
+++ app/src/main/java/com/example/contributorlist/CustomAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.contributorlist
 
-class CustomAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter (private val rowDataList: ArrayList<RowData>, private val listener: AddRowdataListener): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    // Viewの初期化
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView
+        val name: TextView
+        val url: TextView
+
+        init {
+            image = view.findViewById(R.id.contributorsAvatar)
+            name = view.findViewById(R.id.contributorslogin)
+            url = view.findViewById(R.id.contributorsUrl)
+        }
+    }
+
+    // レイアウトの設定
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.list_row, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    // Viewの設定
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val animal = rowDataList[position]
+
+        viewHolder.image.setImageResource(animal.imageId)
+        viewHolder.name.text = animal.name
+        viewHolder.url.text = animal.age.toString()
+    }
+
+    // 表示数を返す
+    override fun getItemCount() = rowDataList.size
 }
