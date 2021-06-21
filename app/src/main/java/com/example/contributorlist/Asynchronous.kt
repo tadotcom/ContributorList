c0b4ca4ca18a0ec79d38419c196eceea543bb9a0
tadotcom <peertopeer1988@gmail.com>
2021-06-21 20:18:42 +0900

    Asynchronousのリファクタリング

--- app/src/main/java/com/example/contributorlist/Asynchronous.kt
+++ app/src/main/java/com/example/contributorlist/Asynchronous.kt
@@ -8,12 +8,11 @@ import java.io.IOException
 import java.io.InputStream
 import java.net.MalformedURLException
 import java.net.URL
-import java.util.*
 
-
-//class Asynchronous() {
 class Asynchronous(_image: ImageView) : AsyncTask<String?, Void?, Bitmap?>() {
+
     private val image: ImageView
+
     override fun doInBackground(vararg params: String?): Bitmap? {
         val image: Bitmap
         return try {
@@ -30,7 +29,6 @@ class Asynchronous(_image: ImageView) : AsyncTask<String?, Void?, Bitmap?>() {
     }
 
     override fun onPostExecute(result: Bitmap?) {
-        // 取得した画像をImageViewに設定します。
         image.setImageBitmap(result)
     }
 
