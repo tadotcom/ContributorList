a0bf4a6c46a5a43de0a2f4a9fae6603b2a54a7e4
tadotcom <peertopeer1988@gmail.com>
2021-06-21 20:27:37 +0900

    ContributorDetailのコメントの修正

--- app/src/main/java/com/example/contributorlist/ContributorDetail.kt
+++ app/src/main/java/com/example/contributorlist/ContributorDetail.kt
@@ -9,11 +9,13 @@ class ContributorDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contributor_detail)
 
-        //フラグメントの表示
         val contributorDetailFragment = ContributorDetailFragment()
 
+        //ContributorDetailFragmentにリスト何番目の行が押されたか値渡しを行う
         val bundle = Bundle()
         bundle.putString("ROWDATA", intent.getStringExtra("ROWDATA"))
+
+        //フラグメントの表示
         contributorDetailFragment.setArguments(bundle)
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.contributor_detail_fragment, contributorDetailFragment)
